3b982d102bf84e36476d55d52622cb377fd372c6
sdianno <sean.dianno@soprasteria.com>
2026-06-02 13:27:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
 
 
 # New section to display smoothie nutrition information
-import requests
-smooothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit.watermelon")
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
 
