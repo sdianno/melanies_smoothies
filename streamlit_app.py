171c74cf57277d5aeb4da52a7d2b76f2da5f8da0
sdianno <sean.dianno@soprasteria.com>
2026-06-02 13:38:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,11 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
-        # New section to display smoothie nutrition information
-        import requests  
-        smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-        st.text(smoothiefroot_response)
+       
+# New section to display smoothie nutrition information
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
 
