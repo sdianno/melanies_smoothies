d9f3e39f8ccf52ebe185a8c61b0fd94dcae7c8ca
sdianno <sean.dianno@soprasteria.com>
2026-06-03 15:45:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ st.write(
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe,use_container_width=True)                                                                     
 st.stop()
 
 ingredients_list = st.multiselect(
